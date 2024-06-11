13f506541ba3b80b3efdd716dbafb1733ed09e2e
kankan222 <jyotikankan222@gmail.com>
2024-06-11 17:31:19 +0530

    deleted unwanted files

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const ejs = require('ejs')
 const indexRouter = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 
 app.use('/', indexRouter)
@@ -13,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.set('view engine', 'ejs')
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
